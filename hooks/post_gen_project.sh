cd357d2fad106d5fd6fb5ba4fc14bed356bdbfcf
Jorre <housenjorre@gmail.com>
2023-03-15 21:34:17 +0100

    updates

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 
 pyenv local
-
 poetry install
 
-git init
-git add .
-git commit -m "generate project with cookiecutter"
+# location of python executable
+EXECUTABLE=$(poetry env info | awk '/Executable/ { print $2; exit }')
+
+# replace the placeholder in the json file
+sed -i.bak "/<interpreter_path>/ s#<interpreter_path>#$EXECUTABLE#" \
+  .vscode/settings.json
+
+# the sed program created a backup (with the `-i.bak` part). Remove it
+rm .vscode/settings.json.bak
--- {{cookiecutter.project_name}}/tests/test_setup.py
+++ {{cookiecutter.project_name}}/tests/test_setup.py
@@ -1,4 +1,4 @@
-from {{cookiecutter.package_name}} import __version__ as version
+from src import __version__ as version
 
 
 def test_version(fake):
