e719065a0ec8c54f4da7bab8956a19354f89d5ef
Jorre <housenjorre@gmail.com>
2023-03-28 23:11:54 +0200

    unit test update

--- {{cookiecutter.project_name}}/tests/test_setup.py
+++ {{cookiecutter.project_name}}/tests/test_setup.py
@@ -2,6 +2,5 @@ from src import __version__ as version
 
 
 def test_version(fake):
-    """Sanity check that there is a version in the right place"""
     assert version == "1.0.0"
     assert fake.pystr() != version
